9bf410dc75316bbe0913f04eb7aa363781f901b7
YouROK <8yourok8@mail.ru>
2026-02-21 02:28:21 +0300

    add skel

---
+++ p2p/services/skeleton/skeleton.go
@@ -0,0 +1,47 @@
+package skeleton
+
+import (
+	"context"
+	"log"
+
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	"github.com/yourok/tunsgo/opts"
+	"github.com/yourok/tunsgo/p2p/models"
+)
+
+type Skeleton struct {
+	host host.Host
+	opts *opts.Options
+	ctx  context.Context
+}
+
+func NewSkeleton(c *models.SrvCtx) *Skeleton {
+	return &Skeleton{
+		host: c.Host,
+		opts: c.Opts,
+		ctx:  c.Ctx,
+	}
+}
+
+func (p *Skeleton) Start() error {
+	log.Println("[P2P] Starting skeleton")
+	return nil
+}
+
+func (p *Skeleton) Stop() {
+	log.Println("[P2P] Stopping URL Proxy")
+}
+
+func (p *Skeleton) Name() string {
+	return "Skeleton"
+}
+
+func (p *Skeleton) ProtocolID() protocol.ID {
+	return "/tunsgo/skeleton/1.0.0"
+}
+
+func (p *Skeleton) HandleStream(stream network.Stream) {
+	defer stream.Close()
+}
