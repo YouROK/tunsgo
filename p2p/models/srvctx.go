71ade03cfd797b088d2ed0a4e0508689b8cae0aa
YouROK <8yourok8@mail.ru>
2026-02-22 20:41:36 +0300

    add global peers

--- p2p/models/srvctx.go
+++ p2p/models/srvctx.go
@@ -2,8 +2,10 @@ package models
 
 import (
 	"context"
+	"sync"
 
 	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/protocol"
 	"github.com/yourok/tunsgo/opts"
 )
@@ -14,4 +16,7 @@ type SrvCtx struct {
 	Ctx        context.Context
 	ProtocolID protocol.ID
 	Slots      chan struct{}
+
+	Peers   map[peer.ID]*PeerInfo
+	MuPeers sync.RWMutex
 }
