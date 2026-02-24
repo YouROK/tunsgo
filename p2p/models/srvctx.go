00ac3e94b71e3b8e86a69b6c1e5c10deb5b9ad5f
YouROK <8yourok8@mail.ru>
2026-02-24 22:25:48 +0300

    add dht rem proto

--- p2p/models/srvctx.go
+++ p2p/models/srvctx.go
@@ -5,17 +5,18 @@ import (
 	"sync"
 
 	"github.com/YouROK/tunsgo/opts"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/core/protocol"
 )
 
 type SrvCtx struct {
-	Host       host.Host
-	Opts       *opts.Options
-	Ctx        context.Context
-	ProtocolID protocol.ID
-	Slots      chan struct{}
+	Host host.Host
+	Opts *opts.Options
+	Ctx  context.Context
+	Dht  *dht.IpfsDHT
+
+	Slots chan struct{}
 
 	Peers   map[peer.ID]*PeerInfo
 	MuPeers sync.RWMutex
