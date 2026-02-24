18719dccc12108b017658ff0d9a459223927a8d7
YouROK <8yourok8@mail.ru>
2026-02-24 22:22:34 +0300

    change status

--- p2p/status.go
+++ p2p/status.go
@@ -16,6 +16,7 @@ type PeerDetail struct {
 	Latency   string    `json:"latency"`
 	LastSeen  time.Time `json:"last_seen,omitempty"`
 	Protocols []string  `json:"protocols"`
+	Hosts     []string  `json:"hosts,omitempty"`
 }
 
 type P2PStatus struct {
@@ -24,8 +25,9 @@ type P2PStatus struct {
 	TotalConns     int                 `json:"total_conns"`
 	TunsPeersCount int                 `json:"tuns_peers_count"`
 	KnownDomains   map[string][]string `json:"known_domains"`
-	Peers          []PeerDetail        `json:"peers_list"`
-	OurPeers       []PeerDetail        `json:"our_peers_list"`
+	Peers          []*PeerDetail       `json:"peers_list"`
+	ConnectedPeers []*PeerDetail       `json:"connected_peers_list"`
+	OurPeers       []*models.PeerInfo  `json:"our_peers_list"`
 }
 
 func (s *P2PServer) Status() *P2PStatus {
@@ -33,11 +35,20 @@ func (s *P2PServer) Status() *P2PStatus {
 		PeerID:       s.host.ID().String(),
 		TotalConns:   len(s.host.Network().Peers()),
 		KnownDomains: make(map[string][]string),
-		Peers:        []PeerDetail{},
+		Peers:        []*PeerDetail{},
 	}
 
 	for _, addr := range s.host.Addrs() {
 		status.ListenAddrs = append(status.ListenAddrs, addr.String())
+	}
+
+	peers := map[peer.ID]*models.PeerInfo{}
+	s.srvctx.MuPeers.RLock()
+	maps.Copy(peers, s.srvctx.Peers)
+	s.srvctx.MuPeers.RUnlock()
+
+	for _, info := range peers {
+		status.OurPeers = append(status.OurPeers, info)
 	}
 
 	connectedPeers := s.host.Network().Peers()
@@ -57,7 +68,7 @@ func (s *P2PServer) Status() *P2PStatus {
 			status.TunsPeersCount++
 		}
 
-		detail := PeerDetail{
+		detail := &PeerDetail{
 			ID:        pID.String(),
 			IsTuns:    isOurNode,
 			Protocols: protocolsString,
@@ -66,11 +77,6 @@ func (s *P2PServer) Status() *P2PStatus {
 		for _, a := range s.host.Peerstore().Addrs(pID) {
 			detail.Addrs = append(detail.Addrs, a.String())
 		}
-
-		peers := map[peer.ID]*models.PeerInfo{}
-		s.srvctx.MuPeers.RLock()
-		maps.Copy(peers, s.srvctx.Peers)
-		s.srvctx.MuPeers.RUnlock()
 
 		if info, ok := peers[pID]; ok {
 			detail.LastSeen = info.LastSeen
@@ -81,7 +87,7 @@ func (s *P2PServer) Status() *P2PStatus {
 
 		status.Peers = append(status.Peers, detail)
 		if isOurNode {
-			status.OurPeers = append(status.OurPeers, detail)
+			status.ConnectedPeers = append(status.ConnectedPeers, detail)
 		}
 	}
 
